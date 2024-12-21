e456761157cbd0e04e193aabf8bc26a78a905aae
mihamik603 <vmm1331@gmail.com>
2024-12-21 21:30:38 +0400

    Update module_14_4.py

--- module_14_4.py
+++ module_14_4.py
@@ -7,86 +7,58 @@ from aiogram.utils import executor
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import sqlite3
 from crud_functions import initiate_db, get_all_products
-
-API_TOKEN = '7592039412:AAG0qyrAiZ86tEtCMA6MNmjzvTpXb9Hy8Gk'  # Укажите Ваш токен
+API_TOKEN = ''
 bot = Bot(token=API_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 initiate_db()
-
-
-
 def add_sample_products():
     conn = sqlite3.connect('products.db')
     cursor = conn.cursor()
-    sample_products = [
-        ("Product1", "Описание 1", 100),
-        ("Product2", "Описание 2", 200),
-        ("Product3", "Описание 3", 300),
-        ("Product4", "Описание 4", 400),
-    ]
+    sample_products = [("Product1", "Описание 1", 100),
+                       ("Product2", "Описание 2", 200),
+                       ("Product3", "Описание 3", 300),
+                       ("Product4", "Описание 4", 400),]
     cursor.executemany('INSERT INTO Products (title, description, price) VALUES (?, ?, ?)', sample_products)
     conn.commit()
     conn.close()
-
-
-# Убедитесь, что эта функция вызывается только один раз
-# add_sample_products()  # Закомментируйте, если уже добавили продукты
-
+# add_sample_products()  # Закомментировать после добавления продуктов
 class UserState(StatesGroup):
     age = State()
     growth = State()
     weight = State()
-
-
 @dp.message_handler(Command('start'))
 async def start(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     buttons = ["Рассчитать", "Информация", "Купить"]
     keyboard.add(*buttons)
     await message.answer("Привет! Я бот, помогающий твоему здоровью.", reply_markup=keyboard)
-
-
 @dp.message_handler(lambda message: message.text == 'Купить')
 async def get_buying_list(message: types.Message):
-    products = get_all_products()  # Получаем все продукты из базы данных
+    products = get_all_products()
     inline_keyboard = InlineKeyboardMarkup()
-
     for product in products:
         title, description, price = product
         inline_keyboard.add(
-            InlineKeyboardButton(title, callback_data=title))  # Используем название продукта как callback_data
+            InlineKeyboardButton(title, callback_data=title)) 
         await message.answer(f'Название: {title} | Описание: {description} | Цена: {price}')
-        # Здесь предполагается, что изображения хранятся в определенной директории
         image_path = f'C:\\Users\\Михалыч\\PycharmProjects\\pythonProject5\\{title}.jpg'
         await message.answer_photo(photo=open(image_path, 'rb'))
-
     await message.answer("Выберите продукт для покупки:", reply_markup=inline_keyboard)
-
-
 @dp.callback_query_handler(lambda call: True)
 async def process_callback(call: types.CallbackQuery):
     product_title = call.data
     await call.answer(f"Вы выбрали продукт: {product_title}")
-
-
-
-
 @dp.message_handler(lambda message: message.text == 'Рассчитать')
 async def main_menu(message: types.Message):
     inline_keyboard = InlineKeyboardMarkup()
-    inline_keyboard.add(
-        InlineKeyboardButton("Рассчитать норму калорий", callback_data='calories'),
-        InlineKeyboardButton("Формулы расчёта", callback_data='formulas')
-    )
+    inline_keyboard.add(InlineKeyboardButton("Рассчитать норму калорий", callback_data='calories'),InlineKeyboardButton("Формулы расчёта", callback_data='formulas'))
     await message.answer("Выберите опцию:", reply_markup=inline_keyboard)
 @dp.callback_query_handler(lambda call: call.data == 'formulas')
 async def get_formulas(call: types.CallbackQuery):
-    formula_message = (
-        "Формула Миффлина-Сан Жеора:\n"
-        "Для мужчин: BMR = 10 * вес(кг) + 6.25 * рост(см) - 5 * возраст(лет) + 5\n"
-        "Для женщин: BMR = 10 * вес(кг) + 6.25 * рост(см) - 5 * возраст(лет) - 161"
-    )
+    formula_message = ( "Формула Миффлина-Сан Жеора:
+    \n""Для мужчин: BMR = 10 * вес(кг) + 6.25 * рост(см) - 5 * возраст(лет) + 5
+    \n""Для женщин: BMR = 10 * вес(кг) + 6.25 * рост(см) - 5 * возраст(лет) - 161")
     await call.message.answer(formula_message)
     await call.answer()
 @dp.callback_query_handler(lambda call: call.data == 'calories')
@@ -114,7 +86,6 @@ async def send_calories(message: types.Message, state: FSMContext):
     calories = 10 * weight + 6.25 * growth - 5 * age + 5
     await message.answer(f"Ваша норма калорий: {calories:.2f} ккал.")
     await state.finish()
-
 @dp.callback_query_handler(lambda call: call.data == 'product_buying')
 async def send_confirm_message(call: types.CallbackQuery):
     await call.message.answer("Вы успешно приобрели продукт!")
