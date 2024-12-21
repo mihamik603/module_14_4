df90e8c4bad7b8c134940d6b9cf95ac65660ebf9
mihamik603 <vmm1331@gmail.com>
2024-12-21 21:26:18 +0400

    Update crud_functions.py

--- crud_functions.py
+++ crud_functions.py
@@ -3,14 +3,7 @@ import sqlite3
 def initiate_db():
     conn = sqlite3.connect('products.db')
     cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS Products (
-            id INTEGER PRIMARY KEY,
-            title TEXT NOT NULL,
-            description TEXT,
-            price INTEGER NOT NULL
-        )
-    ''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Products (id INTEGER PRIMARY KEY,title TEXT NOT NULL,description TEXT,price INTEGER NOT NULL)''')
     conn.commit()
     conn.close()
 
